552fa91e2ae27f5263bb935c7354356e489b5503
juniorworku <juniorworku@gmail.com>
2024-05-24 14:32:47 +0300

    hook setup for kedro

--- kedro-project/src/kedro_project/hooks.py
+++ kedro-project/src/kedro_project/hooks.py
@@ -1,24 +1,22 @@
-from kedro.framework.hooks import hook_impl
-from pyspark import SparkConf
-from pyspark.sql import SparkSession
+# src/kedro_project/hooks.py
 
+from kedro.framework.hooks import hook_impl
+from pyspark.sql import SparkSession
 
 class SparkHooks:
     @hook_impl
     def after_context_created(self, context) -> None:
-        """Initialises a SparkSession using the config
-        defined in project's conf folder.
-        """
+        # Set up a Spark session
+        self.spark = SparkSession.builder \
+            .appName("KedroProject") \
+            .getOrCreate()
 
-        # Load the spark configuration in spark.yaml using the config loader
-        parameters = context.config_loader["spark"]
-        spark_conf = SparkConf().setAll(parameters.items())
+    @hook_impl
+    def before_pipeline_run(self) -> None:
+        # Code to run before the pipeline starts
+        pass
 
-        # Initialise the spark session
-        spark_session_conf = (
-            SparkSession.builder.appName(context.project_path.name)
-            .enableHiveSupport()
-            .config(conf=spark_conf)
-        )
-        _spark_session = spark_session_conf.getOrCreate()
-        _spark_session.sparkContext.setLogLevel("WARN")
+    @hook_impl
+    def after_pipeline_run(self) -> None:
+        # Code to run after the pipeline ends
+        self.spark.stop()
