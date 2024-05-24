06173b2d5cda36883e1fe5248efa063f205845ad
juniorworku <juniorworku@gmail.com>
2024-05-24 11:23:25 +0300

    setting up kedro

---
+++ kedro-project/src/kedro_project/pipelines/data_processing/pipeline.py
@@ -0,0 +1,27 @@
+"""
+This is a boilerplate pipeline 'data_processing'
+generated using Kedro 0.19.5
+"""
+
+# src/kedro_project/pipelines/data_processing/pipeline.py
+
+from kedro.pipeline import Pipeline, node
+from .nodes import process_ads_data, process_play_store_data
+
+def create_pipeline(**kwargs):
+    return Pipeline(
+        [
+            node(
+                func=process_ads_data,
+                inputs="raw_ads_data",
+                outputs="intermediate_ads_data",
+                name="process_ads_data_node",
+            ),
+            node(
+                func=process_play_store_data,
+                inputs="raw_play_store_data",
+                outputs="intermediate_play_store_data",
+                name="process_play_store_data_node",
+            ),
+        ]
+    )
